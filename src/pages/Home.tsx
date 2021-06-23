92f07921b535822a26a1f9cfa26b87c313c2fbda
João Bispo <joaobispo2077@gmail.com>
2021-06-23 12:42:16 -0300

    feat: add navigation flow between Home and NewRoom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,15 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
+import { useHistory } from 'react-router-dom';
 
 export function Home() {
+  const history = useHistory();
+
+  const handleCreateNewRoom = () => {
+    history.push('/rooms/new');
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -17,7 +24,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="letmeask" />
-          <button className="create-rom">
+          <button onClick={handleCreateNewRoom} className="create-rom">
             <img src={googleIconImg} alt="Logo do google" />
             Crie sua sala com o Google
           </button>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
@@ -21,7 +22,7 @@ export function NewRoom() {
             <input type="text" placeholder="Nome da sala" />
             <Button type="submit">Criar sala</Button>
           </form>
-            <p>Quer entrar em uma sala existente? <a href="#">clique aqui</a></p>
+            <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
         </div>
       </main>
     </div>
