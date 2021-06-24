ba395e88aae86cf89e5cb908a88686abbd6ff50f
João Bispo <joaobispo2077@gmail.com>
2021-06-24 15:34:51 -0300

    feat: add structure of room page

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import logoImg from '../assets/images/logo.svg';
+import { Button } from '../components/Button';
+
+import '../styles/room.scss';
 
 export const Room = () => {
   return (
-    <div>
-      Hello
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="let me ask" />
+          <div>codigo</div>
+        </div>
+      </header>
+
+      <main className="content">
+        <div className="room-title">
+          <h1>Sala de perguntas</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form>
+          <textarea placeholder="Oque você quer perguntar?"/>
+
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            <Button type="submit">Enviar pergunta</Button>
+          </div>
+        </form>
+      </main>
     </div>
   )
 }
