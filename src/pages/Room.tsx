c0c56edd700d0eb6a92d2b12128d63f06936704f
João Bispo <joaobispo2077@gmail.com>
2021-06-25 18:13:05 -0300

    feat: add hook useRoom

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -5,6 +5,7 @@ import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
+import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 
 import '../styles/room.scss';
@@ -13,37 +14,15 @@ type RoomParams = {
   id: string;
 }
 
-type FirebaseQuestions = Record<string,{
-  author: {
-    name: string;
-    avatar: string;
-  },
-  content: string;
-  isAnswered: boolean;
-  isHighlighted: boolean;
-}>;
-
-type QuestionState = {
-  id: string;
-  author: {
-    name: string;
-    avatar: string;
-  },
-  content: string;
-  isAnswered: boolean;
-  isHighlighted: boolean;
-}
-
 export const Room = () => {
   const params = useParams<RoomParams>();
   const roomId = params.id;
 
   const { user } = useAuth();
+  const { questions, title } = useRoom(roomId);
 
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<QuestionState[]>([]);
-  const [title, setTitle] = useState('');
-  
+
   const handleSendQuestion = async (event: FormEvent) => {
     event.preventDefault();
 
@@ -69,30 +48,7 @@ export const Room = () => {
     setNewQuestion('');
   }
 
-  useEffect(() => {
-    const roomRef = database.ref(`rooms/${roomId}`);
-    
 
-     roomRef.on('value', room => {
-      const databaseRoom = room.val();
-      
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-
-      const parsedQuestions = Object.entries(firebaseQuestions ).map(([key, value]) => {
-        return { 
-          id: key,
-          content: value.content,
-          author: value.author,
-          isHighlighted: value.isHighlighted,
-          isAnswered: value.isAnswered,
-        };
-      });
-
-      setQuestions(parsedQuestions);
-      setTitle(databaseRoom.title);
-    });
-    
-  }, [roomId]);
 
   return (
     <div id="page-room">
