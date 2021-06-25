772996f9a61c0bd57548c73eb119edef72c7ce09
João Bispo <joaobispo2077@gmail.com>
2021-06-25 19:33:55 -0300

    feat: add structure and visual like

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import "./styles.scss";
 
 type QuestionProps = {
@@ -6,9 +7,10 @@ type QuestionProps = {
     name: string;
     avatar: string;
   },
+  children?: ReactNode
 }
 export const Question = (
-  { content, author }: QuestionProps
+  { content, author, children }: QuestionProps
   ) => {
   return (
     <div className="question">
@@ -18,7 +20,7 @@ export const Question = (
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div></div>
+        <div>{children}</div>
       </footer>
     </div>
   )
